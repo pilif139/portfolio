2dccc4ea79a4c260519a4ef10a099f669cae48be
pilif139 <pifilif@gmail.com>
2026-08-05 01:50:48 +0200

    add icons; add and customize font

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -2,7 +2,10 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
+import icon from 'astro-icon';
+
 // https://astro.build/config
 export default defineConfig({
   vite: { plugins: [tailwindcss()] },
+  integrations: [icon()],
 });
